947ded03a65329038384438848e21315a7a060a1
S.A.M <akhilfilms02@gmail.com>
2025-10-10 02:17:59 -0500

    fix protected route and delete unwanted api files

--- src/components/main-layout.tsx
+++ src/components/main-layout.tsx
@@ -1,9 +1,22 @@
 import { JotaiProvider } from "../components/jotai-provider";
 import { FullscreenDetector } from "../components/fullscreen-detector";
 import { LayoutContent } from "../components/layout-content";
-import { Outlet } from "react-router-dom";
+import { Navigate, Outlet } from "react-router-dom";
+import { useAuth } from "../hooks/useAuth";
 
 export default function MainLayout() {
+  const { isAuthenticated, isLoading } = useAuth();
+
+  // While loading, you can render a loader
+  if (isLoading) {
+    return;
+  }
+
+  // If not authenticated, redirect to login
+  if (!isAuthenticated) {
+    return <Navigate to="/login" replace />;
+  }
+
   return (
     <JotaiProvider>
       <FullscreenDetector />
