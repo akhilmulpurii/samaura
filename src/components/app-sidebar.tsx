afac7feb5389ec1865dab859008d64a0b42e3474
S.A.M <akhilfilms02@gmail.com>
2025-10-12 19:57:53 -0500

    fix typo in logo

--- src/components/app-sidebar.tsx
+++ src/components/app-sidebar.tsx
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 import { useNavigate } from "react-router-dom"; // for React Router
 // @ts-ignore
-import Logo from "../assets/logo/samura.png";
+import Logo from "../assets/logo/samaura.png";
 
 import { useTheme } from "next-themes";
 import {
